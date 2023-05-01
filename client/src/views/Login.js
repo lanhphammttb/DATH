cf57e2a54ec30d41a36a9164e791cdf0749066aa
LanhPham <lanhphammttb@gmail.com>
2023-05-02 00:39:53 +0700

    fix-login
    -----------------------------

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -1,33 +1,64 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { Link} from "react-router-dom";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import Container from "../components/Container";
-import CustomInput from "../components/CustomInput";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
 
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post('http://localhost:8000/login', { username, password })
+      .then((response) => {
+        const token = response.data.token;
+        localStorage.setItem('token', token);
+        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+        window.location.href = '/product'; // Chuyển hướng sang trang dashboard sau khi đăng nhập thành công
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
   return (
     <>
       <Meta title={"Login"} />
       <BreadCrumb title="Login" />
-
       <Container class1="login-wrapper py-5 home-wrapper-2">
         <div className="row">
           <div className="col-12">
             <div className="auth-card">
               <h3 className="text-center mb-3">Login</h3>
-              <form action="" className="d-flex flex-column gap-15">
-                <CustomInput type="email" name="email" placeholder="Email" />
-                <CustomInput
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                />
+              <form onSubmit={handleSubmit} action="" className="d-flex flex-column gap-15" name="formlogin">
+                <input value={username} onChange={(e) => setUsername(e.target.value)} type="email" name="email" placeholder="Email" className=" form-control input" />
+                <div className="d-flex" style={{ backgroundColor: "#F5F5F7", borderRadius: "10px" }}>
+                  <input
+                    value={password} onChange={(e) => setPassword(e.target.value)}
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    className=" form-control input"
+                    id="password"
+                  />
+                  <FontAwesomeIcon style={{ paddingRight: "1rem", cursor: "pointer" }} className="align-self-center" onClick={(e) => {
+                    e.preventDefault();
+                    let a = document.getElementById("password");
+                    a.type === "password" ? a.type = "text" : a.type = "password";
+                  }} icon={faEyeSlash} />
+                </div>
                 <div>
                   <Link to="/forgot-password">Forgot Password?</Link>
 
                   <div className="mt-3 d-flex justify-content-center gap-15 align-items-center">
-                    <button className="button border-0" type="submit">
+                    <button className="button border-0" type="submit"
+                      onClick={(e) => {
+                        // e.preventDefault();
+                      }}>
                       Login
                     </button>
                     <Link to="/signup" className="button signup">
--- server/index.js
+++ server/index.js
@@ -6,12 +6,52 @@ const app = express();
 
 app.use(cors());
 
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    port: '3306',  /* port on which phpmyadmin run */
-    password: '1704',
-    database: 'dath'  
+  host: 'localhost',
+  user: 'root',
+  port: '3306',  /* port on which phpmyadmin run */
+  password: '1704',
+  database: 'dath'  
+});
+
+
+app.get('/users', (req, res) => {
+  connection.query('SELECT * FROM admin', (err, results) => {
+    if (err) {
+      res.status(500).json({ error: err });
+    } else {
+      res.status(200).json({ users: results });
+    }
+  });
+});
+
+const jwt = require('jsonwebtoken');
+const secretKey = 'yoursecretkey';
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  console.log(username + password);
+  connection.query(
+    `SELECT * FROM admin WHERE username = ? AND password = ?`,
+    [username, password],
+    (err, results) => {
+      if (err) {
+        res.status(500).json({ error: err });
+      } else if (results.length === 0) {
+        res.status(401).json({ error: 'Invalid username or password' });
+      } else {
+        const user = results[0];
+        const token = jwt.sign({ id: user.id, username: user.username }, secretKey);
+        res.status(200).json({ token });
+      }
+    }
+  );
 });
 
 connection.connect((err) => {
