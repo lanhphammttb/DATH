3a5b8037762859097c99c62851622cec55ba2ddd
LanhPham <lanhphammttb@gmail.com>
2023-06-06 17:21:14 +0700

    Merge branch 'main' into quang3

--- client/src/components/Admin/Bill/ListBill.js
+++ client/src/components/Admin/Bill/ListBill.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Bill.scss';
-import { useNavigate } from 'react-router-dom';
 
 const ListBill = (props) => {
   const [data, setData] = useState([]);
   useEffect(() => {
     axios
-      .get('http://localhost:8000/api/hoadon')
+      .get('/api/hoadon')
       .then((response) => {
         setData(response.data);
         console.log(response.data);
